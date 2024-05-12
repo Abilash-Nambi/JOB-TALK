9f4e08308156fd9cc37f45da957886f2ed2e9630
Abilash-Nambi <142148604+Abilash-Nambi@users.noreply.github.com>
2024-05-12 15:56:14 +0530

    footer added

--- client/src/Layouts/HomeLayout.jsx
+++ client/src/Layouts/HomeLayout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Footer";
 
 const HomeLayout = () => {
   return (
     <div>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
---
+++ client/src/Pages/Footer.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import logo from "../../public/images/logo.jpg";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer class="bg-white rounded-lg shadow m-4">
+        <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+          <div class="sm:flex sm:items-center sm:justify-between">
+            <Link
+              to="/"
+              class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+            >
+              <img src={logo} className="w-20 h-20" alt="Flowbite Logo" />
+              <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                Flowbite
+              </span>
+            </Link>
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+              <li>
+                <a href="#" class="hover:underline me-4 md:me-6">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#" class="hover:underline me-4 md:me-6">
+                  Privacy Policy
+                </a>
+              </li>
+              <li>
+                <a href="#" class="hover:underline me-4 md:me-6">
+                  Licensing
+                </a>
+              </li>
+              <li>
+                <a href="#" class="hover:underline">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+          <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2023{" "}
+            <Link to="/" class="hover:underline">
+              Job Talk™
+            </Link>
+            . All Rights Reserved.
+          </span>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
