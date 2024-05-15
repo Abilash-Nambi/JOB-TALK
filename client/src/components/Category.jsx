ba611186591fbb1fcca0b2df623a0e0167f62177
Abilash-Nambi <142148604+Abilash-Nambi@users.noreply.github.com>
2024-05-15 21:11:44 +0530

    card style added

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -16,7 +16,21 @@ const Category = () => {
       </div>
       <div className="grid md:grid-cols-4 sm:grid-cols-1 gap-6">
         {jobCategory.map((data) => (
-          <div>{data.name}</div>
+          //   <div>{data.name}</div>
+          <div className="flex justify-center">
+            <div className="block max-w-[18rem] rounded-lg bg-secondary text-primary shadow-secondary-1">
+              <div className=" px-6 py-3">{data.name}</div>
+              <div className="p-6">
+                <h5 className="mb-2 text-xl font-medium leading-tight">
+                  Secondary card title
+                </h5>
+                <p className="text-base">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
         ))}
       </div>
     </div>
