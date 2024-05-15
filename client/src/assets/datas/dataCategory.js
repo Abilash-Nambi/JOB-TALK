7f755070582e16d3d00f7efaa1bbddab256e7164
Abilash-Nambi <142148604+Abilash-Nambi@users.noreply.github.com>
2024-05-15 21:06:57 +0530

    ccategory section doing

---
+++ client/src/assets/datas/dataCategory.js
@@ -0,0 +1,43 @@
+const jobCategory = [
+  {
+    name: "Software Development",
+    jobsAvailable: 150,
+    link: "",
+  },
+  {
+    name: "Data Science",
+    jobsAvailable: 100,
+    link: "",
+  },
+  {
+    name: "Marketing",
+    jobsAvailable: 80,
+    link: "",
+  },
+  {
+    name: "Finance",
+    jobsAvailable: 70,
+    link: "",
+  },
+  {
+    name: "Design",
+    jobsAvailable: 120,
+    link: "",
+  },
+  {
+    name: "Customer Support",
+    jobsAvailable: 90,
+    link: "",
+  },
+  {
+    name: "Human Resources",
+    jobsAvailable: 60,
+    link: "",
+  },
+  {
+    name: "Operations",
+    jobsAvailable: 110,
+    link: "",
+  },
+];
+export default jobCategory;
--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import jobCategory from "../Assets/datas/dataCategory";
 
 const Category = () => {
   const [dataBlock] = useState({
@@ -7,7 +8,17 @@ const Category = () => {
   });
   return (
     <div className="max-w-screen-2xl mx-auto xl:px-24 px-4">
-      <div></div>
+      <div className="md:pt-16 md:pb-10">
+        <h1 className="text-4xl font-bold">Browse by category</h1>
+        <h2 className="text-primary/70 pt-3">
+          Recruitment Made Easy in 100 seconds
+        </h2>
+      </div>
+      <div className="grid md:grid-cols-4 sm:grid-cols-1 gap-6">
+        {jobCategory.map((data) => (
+          <div>{data.name}</div>
+        ))}
+      </div>
     </div>
   );
 };
