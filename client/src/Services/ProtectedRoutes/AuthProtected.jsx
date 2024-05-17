74bba0042c1aaeb7279aaab60076194267b50483
Abilash-Nambi <142148604+Abilash-Nambi@users.noreply.github.com>
2024-05-17 22:53:26 +0530

    bug fixed

--- client/src/Services/ProtectedRoutes/AuthProtected.jsx
+++ client/src/Services/ProtectedRoutes/AuthProtected.jsx
@@ -1,25 +1,3 @@
-// import React from "react";
-// import useLocalStorage from "../../Hooks/useLocalStorage";
-// import useRouter from "../../Hooks/useRouter";
-// import useToast from "../../Hooks/useToast";
-
-// const AuthProtected = ({ children }) => {
-//   const { getStorage } = useLocalStorage();
-//   const { navigate } = useRouter();
-//   const { warningToast } = useToast();
-
-//   const token = getStorage("authToken");
-
-//   if (token) {
-//     return <div>{children}</div>;
-//   } else {
-//     warningToast("Please login");
-//     navigate("/");
-//   }
-// };
-
-// export default AuthProtected;
-
 import React, { useEffect } from "react";
 import useLocalStorage from "../../Hooks/useLocalStorage";
 import useRouter from "../../Hooks/useRouter";
