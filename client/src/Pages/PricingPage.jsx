f628607831f4e244a329734ef1877e7e07d0d7b6
Abilash-Nambi <142148604+Abilash-Nambi@users.noreply.github.com>
2024-05-28 22:17:09 +0530

    color changed

--- client/src/Pages/PricingPage.jsx
+++ client/src/Pages/PricingPage.jsx
@@ -120,14 +120,18 @@ const PricingPage = () => {
               </a>
             </div>
 
-            <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
-              <h3 className="mb-4 text-2xl font-semibold">Standard</h3>
-              <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+            <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-[#f5f5f5]  rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8">
+              <h3 className="mb-4 text-2xl font-semibold text-blue">
+                Standard
+              </h3>
+              <p className="font-light text-primary sm:text-lg ">
                 Relevant for multiple users, extended & premium support.
               </p>
               <div className="flex justify-center items-baseline my-8">
-                <span className="mr-2 text-5xl font-extrabold">₹1500</span>
-                <span className="text-gray-500 dark:text-gray-400">/month</span>
+                <span className="mr-2 text-5xl font-extrabold text-blue">
+                  ₹1500
+                </span>
+                <span className=" text-primary">/month</span>
               </div>
 
               <ul role="list" className="mb-8 space-y-4 text-left">
