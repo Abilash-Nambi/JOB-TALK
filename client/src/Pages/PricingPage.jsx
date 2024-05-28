22840722af4183eaa7353f73c2f0d780492569c9
Abilash-Nambi <142148604+Abilash-Nambi@users.noreply.github.com>
2024-05-28 22:20:08 +0530

    border color chnaged

--- client/src/Pages/PricingPage.jsx
+++ client/src/Pages/PricingPage.jsx
@@ -219,15 +219,17 @@ const PricingPage = () => {
               </ul>
               <a
                 href="#"
-                className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
+                className="text-primary  hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center  "
               >
                 Get started
               </a>
             </div>
 
-            <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
-              <h3 className="mb-4 text-2xl font-semibold">Extended</h3>
-              <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+            <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8  ">
+              <h3 className="mb-4 text-2xl font-semibold text-blue">
+                Extended
+              </h3>
+              <p className="font-light text-primary sm:text-lg ">
                 Best for large scale uses and extended redistribution rights.
               </p>
               <div className="flex justify-center items-baseline my-8">
