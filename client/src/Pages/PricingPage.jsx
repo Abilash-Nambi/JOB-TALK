d06200ae39433085c6a9167cd37fb34e0565b81c
Abilash-Nambi <142148604+Abilash-Nambi@users.noreply.github.com>
2024-05-28 22:11:20 +0530

    txt color chnaged

--- client/src/Pages/PricingPage.jsx
+++ client/src/Pages/PricingPage.jsx
@@ -3,26 +3,28 @@ import React from "react";
 const PricingPage = () => {
   return (
     <div>
-      <section className="bg-white ">
+      <section className="bg-#f5f5f5">
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
           <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold  dark:text-white">
               "Flexible Plans for Your Recruitment Needs
             </h2>
-            <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">
+            <p className="mb-5 font-light text-blue sm:text-xl ">
               Choose the perfect plan to attract top talent and streamline your
               hiring process.
             </p>
           </div>
           <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
-            <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
-              <h3 className="mb-4 text-2xl font-semibold">Basic</h3>
-              <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+            <div className="flex flex-col p-6 mx-auto max-w-lg text-center  bg-[#f5f5f5] rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8  dark:text-white">
+              <h3 className="mb-4 text-2xl font-semibold text-blue">Basic</h3>
+              <p className="font-light text-primary sm:text-lg ">
                 Best option for personal use & for your next project.
               </p>
               <div className="flex justify-center items-baseline my-8">
-                <span className="mr-2 text-5xl font-extrabold">₹450</span>
-                <span className="text-gray-500 dark:text-gray-400">/month</span>
+                <span className="mr-2 text-5xl font-extrabold text-blue">
+                  ₹450
+                </span>
+                <span className="text-primary">/month</span>
               </div>
 
               <ul role="list" className="mb-8 space-y-4 text-left">
