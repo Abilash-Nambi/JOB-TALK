3f2afd1bf54300cf245f8de86576765cc35eab6a
Abilash-Nambi <142148604+Abilash-Nambi@users.noreply.github.com>
2024-05-29 20:13:00 +0530

    price page padding top removed

--- client/src/Pages/PricingPage.jsx
+++ client/src/Pages/PricingPage.jsx
@@ -4,7 +4,7 @@ const PricingPage = () => {
   return (
     <div>
       <section className="bg-#f5f5f5">
-        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl  lg:px-6">
           <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold  dark:text-white">
               "Flexible Plans for Your Recruitment Needs
