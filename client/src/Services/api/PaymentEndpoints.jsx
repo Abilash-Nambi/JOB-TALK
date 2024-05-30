5c916ebf0de1ac2832aa2b4031c93e1aee1f3b7e
Abilash-Nambi <142148604+Abilash-Nambi@users.noreply.github.com>
2024-05-30 12:56:00 +0530

    payment api doing

---
+++ client/src/Services/api/PaymentEndpoints.jsx
@@ -0,0 +1,9 @@
+import { axiosInstance } from "./AxiosInterceptor";
+
+export const orderPayment = async (data) => {
+  const amount = 500;
+  const currency = "INR";
+  const receiptId = "qwsaq1";
+  try {
+  } catch (error) {}
+};
