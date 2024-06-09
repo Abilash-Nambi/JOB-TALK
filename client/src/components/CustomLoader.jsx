8d47b680f34c4834108dbecb2e4c3421df85f93f
Abilash-Nambi <142148604+Abilash-Nambi@users.noreply.github.com>
2024-06-09 18:12:19 +0530

    new spinner added

--- client/src/components/CustomLoader.jsx
+++ client/src/components/CustomLoader.jsx
@@ -1,22 +1,29 @@
 import React from "react";
-import "ldrs/infinity";
+//import "ldrs/infinity";
+import { InfinitySpin } from "react-loader-spinner";
 
 const CustomLoader = () => {
   return (
-    <div
-      aria-live="polite"
-      aria-busy={true}
-      className="items-center flex justify-center h-screen"
-    >
-      <l-infinity
-        size="55"
-        stroke="4"
-        stroke-length="0.15"
-        bg-opacity="0.1"
-        speed="1.3"
-        color="#3575e2"
-      ></l-infinity>
-    </div>
+    // <div
+    //   aria-live="polite"
+    //   aria-busy={true}
+    //   className="items-center flex justify-center h-screen"
+    // >
+    //   <l-infinity
+    //     size="55"
+    //     stroke="4"
+    //     stroke-length="0.15"
+    //     bg-opacity="0.1"
+    //     speed="1.3"
+    //     color="#3575e2"
+    //   ></l-infinity>
+    // </div>
+    <InfinitySpin
+      visible={true}
+      width="200"
+      color="#4fa94d"
+      ariaLabel="infinity-spin-loading"
+    />
   );
 };
 
