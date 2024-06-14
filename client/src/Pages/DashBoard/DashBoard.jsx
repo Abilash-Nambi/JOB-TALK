9358cdc92f7f363cddb55ebe122a28df52b7b52c
Abilash-Nambi <142148604+Abilash-Nambi@users.noreply.github.com>
2024-06-14 23:06:55 +0530

    daata added

--- client/src/Pages/DashBoard/DashBoard.jsx
+++ client/src/Pages/DashBoard/DashBoard.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import DashboardHeader from "../../components/Dashboard/DashboardHeader";
+import DashboardCard from "../../components/Dashboard/DashboardCard";
 
 const DashBoard = () => {
   return (
     <div>
       <DashboardHeader />
+      <div className="p-4 flex md:flex-row flex-col gap-6 justify-center w-full">
+        <DashboardCard title={"Total Jobs"} data={44} />
+        <DashboardCard title={"Total Active Jobs"} data={44} />
+        <DashboardCard title={"Total Not Active Jobs"} data={44} />
+        <DashboardCard title={"Total visited candidates"} data={44} />
+      </div>
     </div>
   );
 };
--- client/src/components/Dashboard/DashboardCard.jsx
+++ client/src/components/Dashboard/DashboardCard.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { FaLongArrowAltUp } from "react-icons/fa";
+const DashboardCard = ({ title, data }) => {
+  return (
+    <div>
+      <div className="h-[150px] w-[250px] bg-white rounded-lg shadow-md p-4 text-center">
+        <h3 className="text-lg font-semibold mb-2">{title}</h3>
 
-const DashboardCard = () => {
-  return <div>DashboardCard</div>;
+        <span className="flex flex-row items-center justify-center">
+          <FaLongArrowAltUp className="text-green-500" />
+          <h1 className="text-5xl text-gray-600">{data}</h1>
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardCard;
